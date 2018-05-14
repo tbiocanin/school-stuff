32f293d01576b7ffa0ad17343586669d00ae83e2
Teodor <tekikralj011@gmail.com>
2018-05-14 10:02:15 +0200

    Lock S-foils in attack position
    even number detection

--- gluposti iz infosa/functions.pas
+++ gluposti iz infosa/functions.pas
@@ -13,8 +13,11 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
     Edit1: TEdit;
+    Edit2: TEdit;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
     { private declarations }
   public
@@ -44,6 +47,30 @@ begin
      input1 := StrToInt(Edit1.Text);
      faktorijel := fact(input1);
      showMessage(IntToStr(faktorijel));
+end;
+
+function par(n: integer) : integer;
+var c, br : integer;
+begin
+  br := 0;
+  while n <> 0 do
+        begin
+          c := n mod 10;
+          if c mod 2 = 0 then
+           begin
+             br := br + 1;
+             //showMessage('broj parnih brojeva je ' + IntToStr(br));
+           end;
+          n := n div 10;
+        end;
+  par := br;
+end;
+procedure TForm1.Button2Click(Sender: TObject);
+var input, even_numbers : integer;
+begin
+  input := StrToInt(Edit1.TExt);
+  even_numbers := par(input);
+  showMessage(IntToStr(even_numbers));
 end;
 
 
