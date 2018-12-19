ac6b447cde6d7ee14b30b775d8d164ffa398f8ca
Teodor <tekikralj011@gmail.com>
2018-12-19 17:21:48 +0100

    did a simple js task

---
+++ udemy courses/js course/example.js
@@ -0,0 +1,6 @@
+alert("Welcome to your bank!");
+
+var deposit = prompt("How much do you want to deposit today?");
+alert("You deposited: " + deposit);
+
+console.log("you are a cool person!")
---
+++ udemy courses/js course/first Exercise/part3.js
@@ -0,0 +1,6 @@
+var lbs= prompt("Enter the value of your weight in lbs: ");
+var kg = lbs * 0.454;
+
+alert("You have " + kg + " kg.");
+
+console.log("Convertion Completed");
