e54934b8596c26bdf41df8ab8d4d77c9ccf46ce7
Teodor Biocanin <tekikralj011@gmail.com>
2019-02-07 22:26:09 +0100

    card game finished

--- udemy courses/python_course/OOP_project.py
+++ udemy courses/python_course/OOP_project.py
@@ -59,24 +59,101 @@ class Hand:
     This is the Hand class. Each player has a Hand, and can add or remove
     cards from that hand. There should be an add and remove card method here.
     '''
-    pass
+    def __init__(self, cards):
+        self.cards = cards
+
+    def __str__(self):
+        return "Contains {} cards".format(len(self.cards))
+
+    def add(self, added_cards):
+        self.cards.extend(added_cards)
+
+    def remove(self):
+        return self.cards.pop()
+
 
 class Player:
     """
     This is the Player class, which takes in a name and an instance of a Hand
     class object. The Payer can then play cards and check if they still have cards.
     """
-    pass
+
+    def __init__(self, name, hand):
+        self.name = name
+        self.hand = hand
+
+    def play_card(self):
+        draw_card = self.hand.remove()
+        print("{} has placed: {}".format(self.name, draw_card))
+        print("\n")
+        return draw_card
+
+    def remove_war_cards(self):
+        war_cards = []
+        for x in range(3):
+            war_cards.append(self.hand.cards.pop())
+        return war_cards
+
+    def still_has_cards(self):
+        """
+        Return True if player still has cards left
+        """
+        return len(self.hand.cards) != 0
+
 
 
 ######################
 #### GAME PLAY #######
 ######################
 print("Welcome to War, let's begin...")
-d = Deck()
+
+#create a new deck and split it in half:
+d=Deck()
 d.shuffle_cards()
 half1, half2 = d.split_cards()
-print(half1)
-print(" ")
-print(half2)
-# Use the 3 classes along with some logic to play a game of war!
+
+#create both players
+comp = Player("computer", Hand(half1))
+
+name = input("What is your name?")
+user = Player(name, Hand(half2))
+
+total_rounds = 0
+war_count = 0
+
+while user.still_has_cards() and comp.still_has_cards():
+    total_rounds+=1
+    print("Time for a new round!")
+    print("Here is the current stanting")
+    print(user.name+"has the counts: " + str(len(user.hand.cards)))
+    print(user.name+"has the counts: " + str(len(comp.hand.cards)))
+    print("Play a card!")
+    print("\n")
+
+    table_cards = []
+
+    c_card = comp.play_card()
+    p_card = user.play_card()
+
+    table_cards.append(c_card)
+    table_cards.append(p_card)
+
+    if c_card[1] == p_card[1]:
+        war_count+=1
+        print("war!")
+
+        table_cards.extend(user.remove_war_cards())
+        table_cards.extend(comp.remove_war_cards())
+
+        if RANKS.index(c_card[1]) < RANKS.index(p_card[1]):
+            user.hand.add(table_cards)
+        else:
+            comp.hand.add(table_cards)
+    else:
+        if RANKS.index(c_card[1]) < RANKS.index(p_card[1]):
+            user.hand.add(table_cards)
+        else:
+            comp.hand.add(table_cards)
+
+print("game over, number of rounds: " + str(total_rounds))
+print("a war happened " + str(war_count)+ " times")
