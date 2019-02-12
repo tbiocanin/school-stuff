98440b3498563ad1206bb4118ff87d16a483b0ad
Teodor Biocanin <tekikralj011@gmail.com>
2019-02-12 18:06:10 +0100

    some url mapping done

--- udemy courses/django_course/firstDjango_application/firstDjango_application/urls.py
+++ udemy courses/django_course/firstDjango_application/firstDjango_application/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path
+from django.conf.urls import include
 from basic_app import views
 
 urlpatterns = [
     url(r'$',views.index, name='index'),
+    url(r'^basic_app/',include('basic_app.urls')),
     path('admin/', admin.site.urls),
 ]
