ece953761a917876afbca274a7f3439bb76e1fc9
Teodor Biocanin <tekikralj011@gmail.com>
2019-02-12 18:59:28 +0100

    added a simple template

--- udemy courses/django_course/firstDjango_application/basic_app/views.py
+++ udemy courses/django_course/firstDjango_application/basic_app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world")
+    my_dict = {'insert_me': "Now I am coming from basic_app/index.html!"}
+    return render(request, 'basic_app/index.html', context=my_dict)
