c8fd607c921363dbfa6adbbb069aabaee52b88ce
Teodor Biocanin <tekikralj011@gmail.com>
2019-04-11 18:13:49 +0200

    shitpost script added

--- Matura project/maturaProject/NewsPage/models.py
+++ Matura project/maturaProject/NewsPage/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class News(models.Model):
-    news_heading = models.CharField(max_length=128)
+    news_heading = models.CharField(max_length=10)
     date = models.CharField(max_length=128)
     text = models.CharField(max_length=10000)
---
+++ Matura project/maturaProject/popNews.py
@@ -0,0 +1,32 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maturaProject.settings')
+
+import django
+django.setup()
+
+import random
+from NewsPage.models import News
+from faker import Faker
+
+fakegen = Faker()
+topics = ['Razmene', 'Takmicejna ucenika', 'Posete osnovnim skolama', 'Obavestenja za ucenike!']
+
+# def add_topics():
+#     t = News.objects.get_or_create(top_name=random.choice(topics))[0]
+#     t.save()
+#     return t
+
+def populate(N=5):
+
+    for entry in range(N):
+        fake_title = fakegen.text()
+        fake_date = fakegen.date()
+        fake_text = fakegen.text()
+
+        news = News.objects.get_or_create(news_heading=fake_title,
+                                          date=fake_date,
+                                          text=fake_text)[0]
+if __name__ == '__main__':
+    print("POPULATING THE DATABASES...PLEASE WAIT")
+    populate(10)
+    print("Populating Complete!")
