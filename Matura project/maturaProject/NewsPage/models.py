11b16de0b4b1de7adf0655d6f112c91a4bda6bea
Teodor Biocanin <tekikralj011@gmail.com>
2019-04-15 12:15:02 +0200

    went from charfield to textfield for news

--- Matura project/maturaProject/NewsPage/models.py
+++ Matura project/maturaProject/NewsPage/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class News(models.Model):
     news_heading = models.CharField(max_length=10)
     date = models.CharField(max_length=128)
-    text = models.CharField(max_length=10000000)
+    text = models.TextField(max_length=10000000)
